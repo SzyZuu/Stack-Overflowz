5b3adf95f8c6dd66e5020af648077cb89dc17e96
I-Protecc <ryanbaumeister@googlemail.com>
2023-12-14 17:30:16 +0100

    Changes by skwar

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,13 +1,10 @@
 package entity;
 
-
-// superclass (parent class)
-
+import main.GamePanel;
 import java.awt.*;
 
 public class Entity {
 
     public int x, y;
     public Point pos = new Point(x, y);
-
 }
