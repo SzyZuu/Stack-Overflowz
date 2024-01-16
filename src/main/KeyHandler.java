bc9e10bff0afaa1362587d67679733a3a5c63a3a
I-Protecc <ryan.baumeister@gmail.com>
2024-01-16 11:28:32 +0100

    feat: buy cards

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -5,18 +5,22 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+    GamePanel gp;
 
+    public KeyHandler(GamePanel gp){
+        this.gp = gp;
+    }
     @Override
     public void keyTyped(KeyEvent e) {
 
     }
-
     @Override
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
 
-        //if(code == KeyEvent.VK_W){        // example bit of code, just to remember how to do key inputs without looking it up
-        // }
+        if(code == KeyEvent.VK_SPACE){        // example bit of code, just to remember how to do key inputs without looking it up
+            gp.buyCard();
+        }
     }
 
     @Override
