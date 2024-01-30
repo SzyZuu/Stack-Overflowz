8234b52b79d10969ad3b53a3f5e54f33016b8d69
SzyZu <skwarek116@gmail.com>
2024-01-30 09:39:14 +0100

    fix: commented out bg color since buggy doc: comments in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,24 +13,24 @@ public class Main {
         window.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
-        GamePanel gamePanel = new GamePanel();
-        c.gridwidth = 3;
-        c.gridx = 0;
+        GamePanel gamePanel = new GamePanel();              //initialize game panel
+        c.gridwidth = 3;                                    //3 grids wide
+        c.gridx = 0;                                        //first grid
         c.gridy = 0;
         //MainThread mainThread = new MainThread(gamePanel, grid);
 
-        window.add(gamePanel, c);
+        window.add(gamePanel, c);                           //add game panel ot the window, with grid constraints
 
-        JPanel tutorialPanel = new JPanel();
-        tutorialPanel.setBackground(Color.BLACK);
-        c.gridwidth = 1;
-        c.gridx = 4;
+        JPanel tutorialPanel = new JPanel();                //new panel for tutorial
+        //tutorialPanel.setBackground(Color.BLACK);         //change background color, but doesnt work
+        c.gridwidth = 1;                                    //only 1 grid wide
+        c.gridx = 4;                                        //4th column first row
         c.gridy = 0;
-        window.add(tutorialPanel, c);
+        window.add(tutorialPanel, c);                       //add tutorial panel, with c
 
         JLabel tutorialLabel = new JLabel();
-        tutorialLabel.setText("<html>Short tutorial:<br>somethingsomethingsomethingsomethingsomethingsomethingsomething</html>");
-        tutorialPanel.add(tutorialLabel);
+        tutorialLabel.setText("<html>Short tutorial:<br>somethingsomethingsomethingsomethingsomethingsomethingsomething</html>");   //use html in label for easier formatting (could've made a separate file for that but too much work and not that necessary)
+        tutorialPanel.add(tutorialLabel);                   //ad the text to the tutorial
 
 
         window.pack(); // causes window to be preferred size and layout
